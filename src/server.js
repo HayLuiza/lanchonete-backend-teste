a4b2d719c0f397932b9431eaecfcd659d15a0419
HayLuiza <hlhabade@gmail.com>
2025-04-01 18:56:13 -0300

    adicionando estrutura inicial

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const path = require('path')
 
+const routes = require('./routes/routes')
+
+const app = express()
+
+app.use(express)
+
+app.use(express.urlencoded({extended: true}))
+
+app.use('/admin', routes)
+
+const port = process.env.PORT||8080
+app.listen(port, () => console.log(`Server listening on port ${port}`))
