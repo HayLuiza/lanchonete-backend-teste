e08a03865c0eb09e2bd39362c8aa0972846ee43c
HayLuiza <hlhabade@gmail.com>
2025-04-16 15:05:23 -0300

    alterando rota e importando cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require ('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -9,7 +10,13 @@ db.connect()
 
 app.use(express.json())
 
-app.use('/admin', routes)
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}))
+
+app.use('/restaurante', routes)
 
 const port = process.env.PORT||8080
 app.listen(port, () => console.log(`Server listening on port ${port}`))
