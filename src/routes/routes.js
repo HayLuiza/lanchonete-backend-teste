c74130d37c6396d206e435935691eedb99ca42f5
HayLuiza <hlhabade@gmail.com>
2025-04-16 14:53:07 -0300

    adicionando rotas clientes, produtos e pedidos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,30 @@
 const router = require('express').Router()
 
-router.get('/clientes', (req, res) => {
-  res.send({
-    ok:123
-  })
-})
+const ClienteController = require('../controllers/cliente')
+const ProdutoController = require('../controllers/produto')
+const PedidoController = require('../controllers/pedido')
+
+// CLIENTE
+router.post('/clientes/login', ClienteController.login)
+router.get('/clientes', ClienteController.get)
+router.post('/clientes', ClienteController.post)
+router.get('/clientes/:id', ClienteController.getById)
+router.put('/clientes/:id', ClienteController.put)
+router.delete('/clientes/:id', ClienteController.remove)
+
+
+//PRODUTO
+router.get('/produtos', ProdutoController.get)
+router.get('/produtos/:id', ProdutoController.getById)
+router.post('/produtos', ProdutoController.post)
+router.put('/produtos/:id', ProdutoController.put)
+router.delete('/produtos/:id', ProdutoController.remove)
+
+
+//PEDIDO
+router.get('/pedidos', PedidoController.get)
+router.post('/pedidos', PedidoController.post)
+router.put('/pedidos/:id', PedidoController.put)
+router.delete('/pedidos/:id', PedidoController.remove)
 
 module.exports = router
